df9d4a7c83a07c3a46ff06a5feb9edf6d5f02477
asonkes <audrey.sonkes@gmail.com>
2023-11-28 10:51:18 +0100

    exercice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-// ici, on a appelé "fragment", pour faire en sorte q'uen dessous, on ne doive pas faire une "div".
-import React, {Component, Fragment} from 'react';
 
-// Si je veux ma CSS
+import React, {Component} from 'react';
 import './App.css'
-
-// On a importer "Membre"... qui vient de "index.js".
 import Membre from './components/Membre';
 import Button from './components/Button';
 
@@ -27,16 +23,22 @@ const league = {
    }
 }
 
-// Cette classe extends, donc elle hérite de "component".
-// Raccourci pour avoir tt ça ==> "cc App".
 class App extends Component {
-// variable d'état, le composant va se recharger, c'est une csorte de storage et je mets les objets dedans et puis on pourra l'utiliser et le modifier.
-  // Ici, mon composant se recharge, sans que je lui dise de le faire... c'est lui qui regarde, s'il y a une modification et lui modifie si nécessaire...
-  // Ecouteur d'évènement...
-
   state = { 
     league: league,
     plus: 2
+   }
+
+   componentDidMount() {
+    console.log('montage')
+   }
+
+   componentDidUpadate() {
+    console.log('je recharge mon composant')
+   }
+
+   componentWillUnmount() {
+    console.log('démontage')
    }
 
   handleClick = (nb) =>{
@@ -45,41 +47,28 @@ class App extends Component {
     this.setState({league})
   } 
 
-  // render = comme si c'était du rendu.
   render() { 
-        // Si je veux un rendu (un return)
-    // Donc ici, comme on a appelé "fragment" en haut, on met fragment au lieu de div et on peut englobé le "h1".
-    // Et on peut par la suite de ne pas mettre "fragment".
+    // Il va me faire un tableau
+    const list = Object.keys(this.state.league).map(iteration => {
+
+      return (
+        // Il va aller voir dans league, s'il y a qqchose qui s'appelle "iteration".
+        // On met "key", au singulier car au-dessus au pluriel mais en-dessous, il veut que chaque objet est unique, donc c'est comme s'il avait un index, donc on met au singulier, au sinon, conflit avec celui du desssus. 
+        <Membre key={iteration} age={this.state.league[iteration].age} nom={this.state.league[iteration].nom}/>
+      )
+    })
 
     return (
       <>
         <h1>Hello World!</h1>
-        <Membre 
-          nom={this.state.league.membre1.nom} 
-          age={this.state.league.membre1.age}
-          />
-        <Membre 
-          nom={this.state.league.membre2.nom} 
-          age={this.state.league.membre2.age}
-          />
-        <Membre 
-          nom={this.state.league.membre3.nom} 
-          age={this.state.league.membre3.age}
-          />
-        <Membre 
-          nom={this.state.league.membre4.nom} 
-          age={this.state.league.membre4.age}
-          />
-
+        {list}
 
         <Button 
           plus={this.state.plus}
           veillir={() => this.handleClick(this.state.plus)}
         />
-
       </>
     )
-   // return React.createElement('div',{className: 'app'}, React.createElement('h1',null,'Hello World'))
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// ReactDOm, c'est le coeur de react, et on crée une racine
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // Ce met en mode développement et permet d'avoir des problèmes dans le site, montre les "warning".
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -13,3 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
